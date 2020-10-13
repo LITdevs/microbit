e3054083ea70f877f6e918700e795ce8b790b18b
Vukky <46850780+Vukky123@users.noreply.github.com>
2020-10-13 10:08:40 +0200

    micro:chat v8

--- microchat/microchat.js
+++ microchat/microchat.js
@@ -1,46 +1,15 @@
 function loader () {
-    basic.pause(100)
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        . . . . .
-        # . . . .
-        `)
-    basic.pause(50)
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        . . . . .
-        # . # . .
-        `)
-    basic.pause(50)
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        . . . . .
-        # . # . #
-        `)
-    basic.pause(50)
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1500)
-    basic.showLeds(`
-        . . . . .
-        # # . # #
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    for (let index = 0; index < 4; index++) {
+        sprite.move(1)
+        basic.pause(100)
+    }
+    for (let index = 0; index < 4; index++) {
+        sprite.move(-1)
+        basic.pause(100)
+    }
 }
 input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Eighth))
     radio.sendValue("mc-a", microchat_ID)
 })
 input.onGesture(Gesture.Shake, function () {
@@ -49,9 +18,11 @@ input.onGesture(Gesture.Shake, function () {
     }
 })
 input.onButtonPressed(Button.AB, function () {
+    music.playTone(262, music.beat(BeatFraction.Eighth))
     radio.sendValue("mc-ab", microchat_ID)
 })
 input.onButtonPressed(Button.B, function () {
+    music.playTone(262, music.beat(BeatFraction.Eighth))
     radio.sendValue("mc-b", microchat_ID)
 })
 radio.onReceivedValue(function (name, value) {
@@ -75,11 +46,13 @@ radio.onReceivedValue(function (name, value) {
         }
     }
 })
+let sprite: game.LedSprite = null
 let microchat_ID = 0
 let booting = 0
 booting = 1
-let version_number = "7"
+let version_number = "8"
 radio.sendString("mc-emu")
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 basic.showLeds(`
     # . . . #
     # . . . #
@@ -89,10 +62,12 @@ basic.showLeds(`
     `)
 microchat_ID = randint(1, 99)
 basic.showString(" micro:chat v" + version_number)
+sprite = game.createSprite(1, 5)
 for (let index = 0; index < randint(2, 4); index++) {
     loader()
 }
-basic.showIcon(IconNames.Fabulous)
+sprite.delete()
+basic.showIcon(IconNames.Yes)
 basic.showString(" Your ID: " + convertToText(microchat_ID))
 booting = 0
 radio.sendValue("mc-s", microchat_ID)
--- microchat/microchat.py
+++ microchat/microchat.py
@@ -1,46 +1,13 @@
 def loader():
-    basic.pause(100)
-    basic.show_leds("""
-        . # . # .
-        . # . # .
-        . . . . .
-        . . . . .
-        # . . . .
-        """)
-    basic.pause(50)
-    basic.show_leds("""
-        . # . # .
-        . # . # .
-        . . . . .
-        . . . . .
-        # . # . .
-        """)
-    basic.pause(50)
-    basic.show_leds("""
-        . # . # .
-        . # . # .
-        . . . . .
-        . . . . .
-        # . # . #
-        """)
-    basic.pause(50)
-    basic.show_leds("""
-        . # . # .
-        . # . # .
-        . . . . .
-        . . . . .
-        . . . . .
-        """)
-    basic.pause(1500)
-    basic.show_leds("""
-        . . . . .
-        # # . # #
-        . . . . .
-        . . . . .
-        . . . . .
-        """)
+    for index in range(4):
+        sprite.move(1)
+        basic.pause(100)
+    for index2 in range(4):
+        sprite.move(-1)
+        basic.pause(100)
 
 def on_button_pressed_a():
+    music.play_tone(262, music.beat(BeatFraction.EIGHTH))
     radio.send_value("mc-a", microchat_ID)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
@@ -50,10 +17,12 @@ def on_gesture_shake():
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 def on_button_pressed_ab():
+    music.play_tone(262, music.beat(BeatFraction.EIGHTH))
     radio.send_value("mc-ab", microchat_ID)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
+    music.play_tone(262, music.beat(BeatFraction.EIGHTH))
     radio.send_value("mc-b", microchat_ID)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
@@ -77,11 +46,13 @@ def on_received_value(name, value):
             basic.show_string(" -" + convert_to_text(value))
 radio.on_received_value(on_received_value)
 
+sprite: game.LedSprite = None
 microchat_ID = 0
 booting = 0
 booting = 1
-version_number = "7"
+version_number = "8"
 radio.send_string("mc-emu")
+music.start_melody(music.built_in_melody(Melodies.POWER_UP), MelodyOptions.ONCE)
 basic.show_leds("""
     # . . . #
     # . . . #
@@ -91,9 +62,11 @@ basic.show_leds("""
     """)
 microchat_ID = randint(1, 99)
 basic.show_string(" micro:chat v" + version_number)
-for index in range(randint(2, 4)):
+sprite = game.create_sprite(1, 5)
+for index3 in range(randint(2, 4)):
     loader()
-basic.show_icon(IconNames.FABULOUS)
+sprite.delete()
+basic.show_icon(IconNames.YES)
 basic.show_string(" Your ID: " + convert_to_text(microchat_ID))
 booting = 0
 radio.send_value("mc-s", microchat_ID)
